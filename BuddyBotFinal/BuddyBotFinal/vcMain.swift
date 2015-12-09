780612219528c5899ea3cf8d205dc30cf80539ec
mateopotato <mateo8correa@gmail.com>
2015-12-09 02:55:23 -0500

    Learns While Talking!
    Things that need to be implemented:
    1. Needs to look for similarity in database, not exact matches
    2. Needs to come pre-trained at least a little bit

--- BuddyBotFinal/BuddyBotFinal/vcMain.swift
+++ BuddyBotFinal/BuddyBotFinal/vcMain.swift
@@ -11,7 +11,7 @@ import CoreData
 
 class vcMain: UIViewController {
     
-    var trainingMode = true;
+    var trainingMode = false;
     
     @IBOutlet var messageField: UITextField!
     @IBOutlet var responseLabel: UILabel!
@@ -55,13 +55,13 @@ class vcMain: UIViewController {
         let managedContext = appDelegate .managedObjectContext
         let entity = NSEntityDescription.entityForName("Phrase", inManagedObjectContext: managedContext)
         
-        var currentMessageKey: String! = ""
+        
+        
+        var messageExists = false
+        var responseExists = false
         
         if(trainingMode) {
-            
-            var messageExists = false
-            var responseExists = false
-            
+            var currentMessageKey: String! = ""
             /* Seperate the training message into the message and the response */
             let fullText: String! = messageField.text
             let convoArr = fullText.componentsSeparatedByString("QQQ")
@@ -131,19 +131,81 @@ class vcMain: UIViewController {
             }
             
         } else {
+            //currently:
+            // 1. looks for the message, gets its key
+            // 2. if key not found, then sets it to 1
+            // 3. adds all the messages that have that key to an array
+            // 4. responds with a random message from the array
+            
+            // what should happen:
+            // 1. Looks for the most similar phrase to the user's message (or one that meets a certain similarity requirement)
+            // --needs to save the uer's message as a new phrase (if it does not already exist) and set it as a response to the computer's message
+            // --if the user's phrase already exists, then it needs to be connected to the  ocmputers's message if it is not already
+            // 2. get the key
+            // 3. add all the messages that have that key to an array
+            // 4. respond with a random message from the array
+            
+            
+            
+            // checking for similarity:
+            // caps dont matter, punctuation doesn't matter,
+            
             
             let messageText = messageField.text
+            var message: NSManagedObject?
+            let computerMessage = responseLabel.text
+            var computerMessageKey = ""
             
+            
+            /* NEEDS TO LOOK FOR A SIMILAR PHRASE AND GET ITS KEY, NOT AN EXACT PHRASE */
             for phrase in phrases {
-                if messageText == (phrase.valueForKey("content") as! String) {
-                    currentMessageKey = (phrase.valueForKey("mesKey") as! String)
+                if messageText == (phrase.valueForKey("content") as! String) { // needs to look for similarity, not exactness
+                    message = phrase
+                    messageExists = true
+                    break
+                }
+            }
+            if message == nil { // get rid of this, when checking for similarity works
+                message = phrases[0]
+            }
+            
+            /* get the mesKey of the computerMessage */
+            for phrase in phrases {
+                if computerMessage == (phrase.valueForKey("content") as! String) {
+                    computerMessageKey = phrase.valueForKey("mesKey") as! String
                 }
             }
             
+            /* create a new message if the message did not exist */
+            if(!messageExists) {
+                let newMessage = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+                newMessage.setValue(messageText, forKey: "content")
+                newMessage.setValue(String(phrases.count + 1), forKey: "mesKey")
+                newMessage.setValue(computerMessageKey, forKey: "resKey1") // saves it as a response for the computer's message
+                
+                do {
+                    try managedContext.save()
+                    phrases.append(newMessage)
+                } catch let error as NSError {
+                    print("Message could not save \(error), \(error.userInfo)")
+                }
+            } else { // message already exists, connect it to the computer message
+                for var index = 1; index < 26; index++ {
+                    if (message!.valueForKey("resKey" + String(index)) as? String) == computerMessageKey {
+                        break // if already connected, break
+                    }
+                    if message!.valueForKey("resKey" + String(index)) == nil {
+                        message!.setValue(computerMessageKey, forKey: "resKey" + String(index))
+                        break // break after connecting
+                    }
+                }
+            }
+            
+            /* responds to the most similar message in the database, regardless of what the user said */
             var allResponses = [String]()
             for phrase in phrases {
                 for var index = 1; index < 26; index++ {
-                    if currentMessageKey == (phrase.valueForKey("resKey" + String(index))) as? String {
+                    if message!.valueForKey("mesKey") as? String == (phrase.valueForKey("resKey" + String(index))) as? String {
                         allResponses.append(phrase.valueForKey("content") as! String)
                     }
                 }
@@ -153,6 +215,7 @@ class vcMain: UIViewController {
             
             responseLabel.text = allResponses[randInt]
         }
+        
     }
     
     @IBAction func btnLoad() {
